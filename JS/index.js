a5dc9cb531e739666da322914a70d3113c6ee69e
Luis Alvarado <juanluiszm.alvarado13@gmail.com>
2023-06-10 23:17:16 -0600

    Actualizacion

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,25 @@
 function cerrarNavbar() {
   $(".navbar-collapse").collapse("hide");
 }
+
+
+
+window.sr = ScrollReveal();
+  sr.reveal('.navbar', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+
+  sr.reveal('.Home', {
+    duration:4000,
+    origin: 'left',
+    distance: '400px'
+  });
+
+  sr.reveal('.School', {
+    duration:4000,
+    origin: 'left',
+    distance: '400px'
+  });
+
