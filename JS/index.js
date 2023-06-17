34c939ef3f88e86579af30d6aa44e465b4938f62
Luis Alvarado <juanluiszm.alvarado13@gmail.com>
2023-06-17 17:34:59 -0600

    Se agregaron estilos

--- JS/index.js
+++ JS/index.js
@@ -23,3 +23,34 @@ window.sr = ScrollReveal();
     distance: '400px'
   });
 
+  sr.reveal('.title', {
+    duration:4000,
+    origin: 'left',
+    distance: '400px'
+  });
+
+  // Social Media
+  function toggleBtn() {
+    const Btns = document.querySelector(".btns");
+    const add = document.getElementById("add");
+    const remove = document.getElementById("remove");
+    const btn = document.querySelector(".btns").querySelectorAll("a");
+    Btns.classList.toggle("open");
+    if (Btns.classList.contains("open")) {
+      remove.style.display = "block";
+      add.style.display = "none";
+      btn.forEach((e, i) => {
+        setTimeout(() => {
+          bottom = 40 * i;
+          e.style.bottom = bottom + "px";
+          console.log(e);
+        }, 100 * i);
+      });
+    } else {
+      add.style.display = "block";
+      remove.style.display = "none";
+      btn.forEach((e, i) => {
+        e.style.bottom = "0px";
+      });
+    }
+  }
